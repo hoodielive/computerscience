9f3b647275b3a0c2f0a914e92bc9bc7cd3919cf4
hoodielive <hoodielive@gmail.com>
2018-12-30 18:54:43 -0500

    define array time complexity

---
+++ javascript/Big-O-Notation/arrays-ordered-content.js
@@ -0,0 +1,24 @@
+/** 
+ * Arrays are used when you need:
+ * Order 
+ * When you need fast access/insertion and removal (sort of...)
+ * Insertion - It depends..
+ * Removal - It depends 
+ * Access - O(1) constant
+ * Searching - O(N) linear
+ * push - O(1) - insert (back)
+ * push - O(N) - insert (front)
+*/ 
+
+/** 
+ * Operations 
+ * push O(1)
+ * pop O(1)
+ * shift O(N)
+ * unshift O(N)
+ * concat O(N)
+ * slice O(N)
+ * splice O(N)
+ * sort O(N * log N)
+ * forEach/map/filter/reduce O(N)
+*/ 
