d68cc77b9f63cd549f4d9f8630f2ceba2c28e68f
hoodielive <hoodielive@gmail.com>
2018-12-30 18:40:22 -0500

    objects are quick, properties can slow them down

---
+++ javascript/Big-O-Notation/objects.js
@@ -0,0 +1,26 @@
+/** 
+ * Object is an unordered, key value pair
+ * Fast insertion/access/removal
+*/
+
+let student = {
+  firstName: "Lawrence", 
+  isStudent: true,
+  favoriteNumbers: [1,2,3,4,5]
+}
+
+/** 
+ * insertion O(1) - constant time
+ * removal O(1) - constant time
+ * searching O(N) - linear time
+ * access O(1) - constant time
+*/
+
+/** 
+ * Big O of Object Methods 
+ * Object.keys - O(N)
+ * Object.values - O(N)
+ * Object.entries - O(N)
+ * hasOwnProperty - O(1)
+*/
+
