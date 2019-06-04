8afc3349901fb8fd5cc491cd87d9235a47d39730
hoodielive <hoodielive@gmail.com>
2019-06-03 21:07:09 -0400

    ruby - big o notation search

--- ruby/big-o-notation.rb
+++ ruby/big-o-notation.rb
@@ -9,3 +9,20 @@ def search(name_to_find)
 end
 
 search('Larry')
+
+# write it as a binary search 
+
+def search(name_to_find, from = 0, to = nil)
+  to ||= @people.count - 1
+  mid = (from + to) / 2 
+
+  if name_to_find < @people[mid].name
+    search(@people, value, from, mid-1)
+  elsif name_to_find > @people[mid].name 
+    search(@people, value,  mid+1, to)
+  else 
+    @people[mid]
+  end
+end
+
+search('Larry')
