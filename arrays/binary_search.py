a83342e87b6cb2ed02c0c08df9c20f39ee451ece
hoodielive <hoodielive@gmail.com>
2019-06-23 11:41:21 -0400

    fixed array runtime

---
+++ arrays/binary_search.py
@@ -0,0 +1,9 @@
+# Does 19 exist? 
+# binary search log(n) (log of n) 
+
+alist = [1,5,7,8,10,12,17,19,20,40,52]
+
+# L + R / 2 = 0 + 10 / 2  = 5 
+# what is x of 5 and is it greater or less than our target (19)?
+# x(5) => 12 ? 
+
