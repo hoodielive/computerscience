e88b77d81c3c9efd75fbc38ee72a14c3f25bbb3c
hoodielive <hoodielive@gmail.com>
2019-06-25 21:15:31 -0400

    http-server

---
+++ computation/http-server.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+def main
+	socket = Socket.new(:INET, :STREAM)
+	socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+	socket.bind(Addrinfo.tcp("127.0.0.1", 9000))
+	socket.listen(0)
+	conn_sock, addr_info = socket.accept
+end
+
+main
